abde66f769dc079693e55865fb9ae591c9ed02e5
Lance Whatley <whatl3y@gmail.com>
2016-12-07 14:18:20 -0500

    URL Generator and preview

--- src/public/js/app/controllers/global_controller.js
+++ src/public/js/app/controllers/global_controller.js
@@ -4,11 +4,13 @@ export default class GlobalController {
 
     this._scope.optionKey = this.optionKey.bind(this)
     this._scope.isArray = this.isArray.bind(this)
+    this._scope.validateImageSettings = this.validateImageSettings.bind(this)
     this._scope.updateAryLength = this.updateAryLength.bind(this)
     this.init()
   }
 
   init() {
+    this.baseUrl = `${location.protocol}//${location.host}`
     this._scope.processingFunctions = []
     this._scope.functionTypes = {
       b: {name: 'Add Border', options: [{length:'number'}]},
@@ -20,6 +22,59 @@ export default class GlobalController {
       s: {name: 'Resize Image of specified width, having same width:height ratio', options: [{width: 'number'}]},
       q: {name: 'Make image square by cropping from the middle outward', options: []}
     }
+
+    this.validateImageSettings()
+    setInterval(() => this._scope.$apply(() => this.validateImageSettings()),1000)
+    // this._scope.$watch(this._scope.imageUrl, (i) => this.validateImageSettings())
+    // this._scope.$watch(this._scope.processingFunctions, (i) => this.validateImageSettings())
+  }
+
+  generateUrl(imageUrl, settings) {
+    const encodedFunctions = settings.map((s) => s.type).join('') || '*'
+    let encodedSettings = {}
+
+    settings.forEach((s) => {
+      encodedSettings[s.type] = {}
+      const typeConfig = this._scope.functionTypes[s.type]
+      if (typeConfig) {
+        typeConfig.options.forEach((o) => {
+          const key = Object.keys(o)[0]
+          encodedSettings[s.type][key] = s[key].val
+        })
+      }
+    })
+    encodedSettings = (Object.keys(encodedSettings).length) ? encodeURIComponent(JSON.stringify(encodedSettings)) : '*'
+
+    this._scope.generatedUrl = `${this.baseUrl}/${encodedFunctions}/${encodedSettings}/${imageUrl}`
+  }
+
+  validateImageSettings() {
+    if (!this.validateUrl(this._scope.imageUrl)) return this._scope.validateError = 'Please make sure you have a valid image URL (starting with http:// or https://)'
+    if (!this.validateSettings(this._scope.processingFunctions)) return this._scope.validateError = 'Please ensure all processing functions selected have required values configured.'
+    delete(this._scope.validateError)
+    return this.generateUrl(this._scope.imageUrl, this._scope.processingFunctions)
+  }
+
+  validateUrl(url) {
+    return url && (url.indexOf('http://') === 0 || url.indexOf('https://') === 0)
+  }
+
+  validateSettings(settings) {
+    let isValid = true
+    settings.forEach((setting) => {
+      const typeConfig = this._scope.functionTypes[setting.type]
+      if (typeConfig) {
+        typeConfig.options.forEach((o) => {
+          const key = Object.keys(o)[0]
+          if (!setting[key] || (!setting[key].val && setting[key].val !== 0)) {
+            isValid = false
+          }
+        })
+      } else {
+        isValid = false
+      }
+    })
+    return isValid
   }
 
   optionKey(optionsObj) {
