8646f854f99c52b9b2f02804f17a7e91cee9f1d8
Lance Whatley <whatl3y@gmail.com>
2016-12-20 22:39:11 -0500

    Upgrade nodejs version and handle invalid objects from S3

--- routes/0.._file_s3_[star]..get.js
+++ routes/0.._file_s3_[star]..get.js
@@ -2,6 +2,7 @@ var async = require('async')
 var streamifier = require('streamifier')
 var AWS = require('aws-sdk')
 var config = require('../config')
+var log = require('bunyan').createLogger(config.logger.options)
 
 var s3 = new AWS.S3()
 
@@ -9,5 +10,10 @@ module.exports = function(req,res) {
   var bucket = config.aws.s3.bucket
   var filename = req.params[0]
   var params = {Bucket: bucket, Key: filename}
-  s3.getObject(params).createReadStream().pipe(res)
+  s3.getObject(params).createReadStream()
+    .on('error', (err,response) => {
+      res.status(500).json(err.toString())
+      return log.error(err)
+    })
+    .pipe(res)
 }
